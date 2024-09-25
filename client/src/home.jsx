ddc2eff476c03dc80d7305ac52a8ba43c7969432
Kanna Neeraja <neerajasesa86@gmail.com>
2024-09-26 01:33:04 +0530

    Initial commit

---
+++ client/src/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function Home(){
+    return(
+        <div>
+            <h2>
+                Home component
+            </h2>
+            <p> Dashboard</p>
+        </div>
+    )
+}
+export default Home; 
